93159233dda81185fe3a8dcb6ba90604286f453f
Tsai <497925132@qq.com>
2020-12-15 14:59:57 +0800

    '2020-12-15提交'

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="goods-item">
-    <img :src="showImage" alt="" @load="imageLoad" >
+<!--    <img :src="showImage" alt="" @load="imageLoad" >-->
+<!--    图片懒加载，vue-lazyload-->
+    <img v-lazy="showImage" alt="" @load="imageLoad" >
     <div class="goods-info">
       <p>{{goodsItem.title}}</p>
       <span class="price">{{goodsItem.price}}</span>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import App from './App.vue'
 import router from './router'
 // 挂载vuex
 import store from './store/index'
+// 导入移动端点击延迟消除
+import FastClick from 'fastclick'
 
 // 一开始就导入toast，默认导入的是index.js的toast
 import toast from 'components/common/toast'
+// 懒加载插件
+import VueLazyload from "vue-lazyload";
 
 Vue.config.productionTip = false
 
@@ -20,3 +24,11 @@ new Vue({
   router,
   store
 }).$mount('#app')
+
+// 解决移动端300ms延迟
+FastClick.attach(document.body)
+// 使用懒加载的插件
+// 使用插件时传参loading-占位图
+Vue.use(VueLazyload, {
+  loading: require('./asset/img/common/placeholder.png')
+})
